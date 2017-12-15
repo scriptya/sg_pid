77222ec4970545253106a141acd746360dae18c0
Unknown <scriptgalih@gmail.com>
2017-12-15 16:25:26 +0700

    Normal & Integral Mode

---
+++ SG_PID.h
@@ -0,0 +1,136 @@
+/*
+   v0.01
+   created by
+   2017 scriptgalih
+
+   Contact us : scriptgalih@gmail.com
+
+   Departemen Teknik Elektro dan Informatika
+   Sekolah Vokasi
+   Universitas Gadjah Mada
+   Yogyakarta
+
+*/
+
+#define INTEGRAL 1
+#define NORMAL 2
+
+class pid {
+    /*
+      sv = set point
+      pv = pembacaan sensor
+      e  = error
+      Kp = Gain Proportional
+      Ki = Gain Integral
+      Kd = Gain  Derivative
+      Tc = Time Sampling (ms)
+      mode = dU intergal / normal
+    */
+    float sv = 0, pv = 0, e, integralE, lastE, derivativeE, Kp = 0, Ki = 0, Kd = 0, dU = 0;
+    int syntaxError = 0;
+    unsigned long previousMillis = 0;
+    float outP, outI, outD;
+    float maxOut = 0;
+    float minOut = 0;
+    long lastTimeTC;
+    float outPID, Tc;
+    int mode;
+    int first = 1;
+  public:
+    void param(float  Kp, float  Ki, float  Kd, int mode);
+    void setPoint(float sv);
+    void constraint(float minOut, float maxOut);
+    void readSensor(float pv);
+    void timeSampling(int Tc);
+    float showPID() {
+      return outPID;
+    }
+    void showUnitPID(){
+      Serial.print(outP); Serial.print("\t");
+      Serial.print(outI); Serial.print("\t");
+      Serial.print(outD); Serial.print("\t");
+    }
+    void showParam() {
+      Serial.print(" Kp:"); Serial.print(Kp);
+      Serial.print(" Ki:"); Serial.print(Ki);
+      Serial.print(" Kd:"); Serial.print(Kd);
+      Serial.print(" Tc:"); Serial.print(Tc);
+      Serial.print("ms mode:");
+      if (mode == 1) {
+        Serial.print("INTEGRAL");
+      } else {
+        Serial.print("NORMAL");
+      }
+    }
+
+    int calc() {
+      //error calculation
+      if (syntaxError == 1)while (1);
+      e = sv - pv;
+      //Derivative protection spike value
+      if (first == 1) {
+        first = 0;
+        derivativeE = e;
+      }
+      unsigned long Time = micros();
+      if (Time - lastTimeTC >= (Tc * 1000)) {
+        float tc =  (Time - lastTimeTC) / 1000000.0;
+        lastTimeTC = Time;
+        //Proportional calculation
+        outP = Kp * e;
+        //Integral calculation
+        integralE = e + lastE;
+        outI = Ki * integralE * tc; //nilai Ki ditentukan melalui tuning
+        lastE = integralE;
+
+        //Derivative calculation
+        derivativeE = e - derivativeE;
+        outD = (Kd * derivativeE) / tc; //nilai Kd ditentukan melalui tuning
+        derivativeE = e;
+        dU = outP + outI + outD;
+        if (mode == 1) {
+          outPID += dU;
+        } else {
+          outPID = dU;
+        }
+        if (maxOut != 0 || minOut != 0) {
+          if (outPID >= maxOut) {
+            outPID = maxOut;
+            integralE -= e;
+          } else if (outPID < minOut) {
+            outPID = minOut;
+            integralE += e;
+          }
+        }
+
+        return outPID;
+      }
+    }
+};
+
+void pid::param(float  vKp, float  vKi, float  vKd, int vmode) {
+  Kp = vKp;
+  Ki = vKi;
+  Kd = vKd;
+  mode = vmode;
+}
+
+void pid::readSensor(float vPv) {
+  pv = vPv;
+}
+
+void pid::setPoint(float sp) {
+  sv = sp;
+}
+void pid::timeSampling( int ts) {
+  Tc = ts;
+}
+
+void pid::constraint(float Min, float Max) {
+  minOut = Min;
+  maxOut = Max;
+  if (minOut > maxOut) {
+    Serial.println("Error 1 \nThe value of min don't over velue of max \n.constraint(min,max)");
+    syntaxError = 1;
+  }
+}
---
+++ example/PID/PID.ino
@@ -0,0 +1,48 @@
+/*
+  SCRIPTGALIH PID v0.01
+  created by
+  2017 Galih Setyawan
+
+  how to use?
+  .param(Kp, Ki, Kd, MODE); NORMAL or INTEGRAL
+  .constraint(min output PID, max output PID);
+  .setPoint(set point PID);
+  .readSensor(value of output);
+  .timeSampling(time sampling in ms);
+  .showParam(); <- this function just for print parameter (Kp,Ki,Kd,TimeSampling,mode)
+  .showUnitPID(); <- print output P, I, and D
+  don't use delay()!
+  
+  Contact us : scriptgalih@gmail.com
+
+  Universitas Gadjah Mada
+  Yogyakarta
+
+*/
+
+#include "SG_PID.h"
+
+pid balancing;
+void setup() {
+  Serial.begin(115200);
+  balancing.param(5, 4, 0.02, NORMAL);
+  balancing.constraint(-5000, 5000);
+  balancing.setPoint(0);
+  balancing.readSensor(0);
+  balancing.timeSampling(1);
+  balancing.showParam();
+
+}
+
+void loop() {
+  int valSensor = random(-20, 20);
+  balancing.readSensor(valSensor);
+  
+  Serial.print(valSensor); Serial.print("\t");
+  balancing.calc();
+
+
+  balancing.showUnitPID();
+  Serial.print(balancing.showPID(), 4);
+  Serial.println();
+}
